55c94dff9086485be295fc5c8d2ac3ca534fea61
simone <simo.x@outlook.it>
2025-12-27 01:49:14 +0100

    General commit

--- src/main/java/it/SimoSW/model/Appointment.java
+++ src/main/java/it/SimoSW/model/Appointment.java
@@ -14,6 +14,7 @@ public class Appointment {
     public Appointment() {
     }
 
+
     public Appointment(long id, long patientId, long therapistId,
                        LocalDateTime start, LocalDateTime end) {
         this.id = id;
